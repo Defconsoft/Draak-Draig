3bc20557e0127d2d63d04c7957acef0e07142692
Rob Charlish <rob.charlish@gmail.com>
2022-11-13 11:18:09 +0000

    First Person Movement and camera started

---
+++ Assets/MainProject/Scripts/Player/PlayerController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+
+    [Header ("PlayerAttributes")]
+    [SerializeField] private bool groundedPlayer;
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private InputManager inputManager;
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        inputManager = InputManager.Instance;
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movement = inputManager.GetPlayerMovement();
+        Vector3 move = new Vector3 (movement.x, 0f, movement.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (inputManager.PlayerJumpedThisFrame() && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        if (inputManager.PlayerInteractThisFrame()) {
+
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
