3ba9a3ac68c079506d586e65d7dfa98c542c81a7
Rob Charlish <rob.charlish@gmail.com>
2022-11-13 12:33:36 +0000

    More Camera work

--- Assets/MainProject/Scripts/Player/PlayerController.cs
+++ Assets/MainProject/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Cinemachine;
 
 
 [RequireComponent(typeof(CharacterController))]
@@ -10,47 +11,71 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float playerSpeed = 2.0f;
     [SerializeField] private float jumpHeight = 1.0f;
     [SerializeField] private float gravityValue = -9.81f;
-
+    public bool Interacting;
 
     private CharacterController controller;
     private Vector3 playerVelocity;
     private InputManager inputManager;
+    private Transform cameraTransform;
+
+    [Header ("Cameras")]
+    public CinemachineVirtualCamera FirstPersonCam;
+    public CinemachineVirtualCamera InteractCam;
+    
 
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
         inputManager = InputManager.Instance;
+        cameraTransform = Camera.main.transform;
     }
 
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
+
+
+        //Move Player if not interacting
+        if (!Interacting){
+            Cursor.visible = false;
+            groundedPlayer = controller.isGrounded;
+            if (groundedPlayer && playerVelocity.y < 0)
+            {
+                playerVelocity.y = 0f;
+            }
+
+            Vector2 movement = inputManager.GetPlayerMovement();
+            Vector3 move = new Vector3 (movement.x, 0f, movement.y);
+            move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+            move.y = 0f;
+            controller.Move(move * Time.deltaTime * playerSpeed);
+
+            // Changes the height position of the player..
+            if (inputManager.PlayerJumpedThisFrame() && groundedPlayer)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            }
+
+            if (inputManager.PlayerInteractThisFrame()) {
+                Interacting = true;
+                FirstPersonCam.m_Priority = 0;
+                InteractCam.m_Priority = 10;
+            }
+
+            playerVelocity.y += gravityValue * Time.deltaTime;
+            controller.Move(playerVelocity * Time.deltaTime);
+
+        } else {
+
+            Cursor.visible = true;
+
+
         }
 
-        Vector2 movement = inputManager.GetPlayerMovement();
-        Vector3 move = new Vector3 (movement.x, 0f, movement.y);
-        controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
 
-        // Changes the height position of the player..
-        if (inputManager.PlayerJumpedThisFrame() && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
 
-        if (inputManager.PlayerInteractThisFrame()) {
 
-        }
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
