fceddd46cf35cdafd5d03adfc1206f71181161dd
aadutr <aadutree@gmail.com>
2023-02-24 14:59:03 +0100

    Added in eye type selection and changes selection frame

--- Assets/MainProject/Scripts/UX/ShaderCustomization.cs
+++ Assets/MainProject/Scripts/UX/ShaderCustomization.cs
@@ -6,6 +6,8 @@ public class ShaderCustomization : MonoBehaviour
 {
     // public SkinnedMeshRenderer skinnedMeshRenderer;
     public Material material; 
+    public Texture[] textures;
+    public GameObject[] frames;
 
     
     public void ApplyHueChange(System.Single val)
@@ -16,5 +18,22 @@ public class ShaderCustomization : MonoBehaviour
     public void ApplySaturationChange(System.Single val)
     {
         material.SetFloat("_Saturation", val);
+    }
+
+    public void ChangeTexture(int idx)
+    {
+        material.SetTexture("_baseTexture", textures[idx]);
+        // Update selection frame
+        foreach (GameObject go in frames)
+        {
+            if (go == frames[idx])
+            {
+                go.SetActive(true);
+            }
+            else
+            {
+                go.SetActive(false);
+            }
+        }
     }
 }
